392447e280b0823a7e84e61a7d1611d6e62d5572
palash1512 <palash9515@gmail.com>
2019-04-24 12:56:55 +0530

    Add register and login page functional with database

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
-from flask import render_template
+from flask import render_template, flash, redirect
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
+
 from app import app
+from app.forms import LoginForm
+
+from app import db
+from app.forms import RegistrationForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +25,39 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
